80e4af6a3e46627a30204102528112ab65b87643
Tim van den Enden <te.vandenenden@student.avans.nl>
2022-12-09 12:19:40 +0100

    Added home page design v1

--- frontend/src/pages/_shared/template.js
+++ frontend/src/pages/_shared/template.js
@@ -1,11 +1,25 @@
 import './styles.css';
+// import logo from './_images/InHolland.png';
 
 function Template(props) {
-    return (
-        <>
-            { props.children }
-        </>
-    );
+    return (<>
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand nunito" href="#">
+                <span className="brand-name main-color">Inholland</span>
+                <span className="secondary-color"> / AI</span>
+            </a>
+        </nav>
+
+        <main>
+            <div className="container">
+                {props.children}
+            </div>
+        </main>
+        <footer>
+            Ontwikkeld door Krokante Krab - 2022
+        </footer>
+
+    </>);
 }
 
 export default Template;
