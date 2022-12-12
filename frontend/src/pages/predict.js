ae856785920555ce27ee577d1fbd0075b836fd0d
Tim van den Enden <te.vandenenden@student.avans.nl>
2022-12-12 12:33:18 +0100

    Made request working

--- frontend/src/pages/predict.js
+++ frontend/src/pages/predict.js
@@ -14,8 +14,25 @@ function Predict() {
         inputRef.current.click();
     };
 
-    const handleSubmit = () => {
-        setResult(true);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        let formData = new FormData();
+        formData.append('patient_xml', inputRef.current.files[0]);
+
+        const requestOptions = {
+            // headers: {'Content-Type': 'application/json'},
+            method: 'POST',
+            body: formData,
+        };
+
+        fetch('http://localhost:3000/api/predict/parodontitis', requestOptions)
+            .then((response) =>
+                response.json())
+            .then((data) => {
+                console.log(data);
+                setResult(data)
+            });
     }
 
     const handleFileChange = event => {
@@ -25,7 +42,7 @@ function Predict() {
         }
 
         // 👇️ reset file input
-        event.target.value = null;
+        // event.target.value = null;
 
         // 👇️ can still access file object here
         setLabel(fileObj.name);
@@ -35,27 +52,30 @@ function Predict() {
     return (<>
 
         { result === undefined &&
-            <div className="row">
-                <div className="col-12">
-                    <h1 className="main-color nunito title">Upload een EPD</h1>
-                    <p className="under-title">
-                        Het elektronisch patiëntendossier (EPD) is verplicht om als xml te worden geupload.
-                    </p>
+            <form onSubmit={ handleSubmit }>
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="main-color nunito title">Upload een EPD</h1>
+                        <p className="under-title">
+                            Het elektronisch patiëntendossier (EPD) is verplicht om als xml te worden geupload.
+                        </p>
 
-                    <div className="dropzone" onClick={ handleClick }>
-                        <div className="dropzone-text">
-                            <i className={ icon + " dropzone-icon" }/>
-                            <div className="dropzone-text nunito">{ label }</div>
+                        <div className="dropzone" onClick={ handleClick }>
+                            <div className="dropzone-text">
+                                <i className={ icon + " dropzone-icon" }/>
+                                <div className="dropzone-text nunito">{ label }</div>
+                            </div>
+
+                            <input className="dropzone-input" type="file" accept=".xml" ref={ inputRef } onChange={ handleFileChange }/>
                         </div>
-                        <input id="dropzone-input" type="file" ref={ inputRef } onChange={ handleFileChange }/>
+
+                        <button type="submit" className="btn btn-primary nunito">
+                            Upload!
+                        </button>
+
                     </div>
-
-                    <button type="button" className="btn btn-primary nunito" onClick={ handleSubmit }>
-                        Upload!
-                    </button>
-
                 </div>
-            </div>
+            </form>
         }
 
         { result !== undefined &&
@@ -63,8 +83,16 @@ function Predict() {
                 <div className="col-12">
                     <h1 className="main-color nunito title">Resultaat</h1>
                     <p className="under-title">
-                        Lorum ipsum.............
+                        { result.prediction['has-not-parodontitis'] > result.prediction['has-parodontitis'] &&
+                            <>De patient heeft <b>geen</b> parodontitis! ({ result.prediction['has-not-parodontitis'] }%)</>
+                        }
+
+                        { result.prediction['has-parodontitis'] > result.prediction['has-not-parodontitis'] &&
+                            <>De patient heeft <b>wel</b> parodontitis! ({ result.prediction['has-parodontitis'] }%)</>
+                        }
                     </p>
+
+                    <img className="shap-img" src={"data:image/png;base64, " + result['shap-img']} alt="shap"/>
                 </div>
             </div>
         }
