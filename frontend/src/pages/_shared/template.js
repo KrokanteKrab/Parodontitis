5f9dc7251d430548a4d815f0e1b8d5a1e67fb2f7
Tim van den Enden <te.vandenenden@student.avans.nl>
2022-12-19 10:52:18 +0100

    New way for nicer css

--- frontend/src/pages/_shared/template.js
+++ frontend/src/pages/_shared/template.js
@@ -1,15 +1,25 @@
-import './styles.css';
+import './main.css';
 import { Link } from "react-router-dom";
 import logo from './_images/logo-inholland.png';
 
 function Template(props) {
     return (<>
-        <div className="container">
-            <nav className="navbar navbar-light bg-light">
-                <Link className="navbar-brand nunito" to="/">
-                    <img className="navbar-logo" src={ logo } alt="logo"/>
-                </Link>
-            </nav>
+        <div className="nav-holder">
+
+            <div className="container">
+                <nav className="navbar navbar-light bg-light">
+                    <Link className="navbar-brand nunito" to="/">
+                        <img className="navbar-logo" src={ logo } alt="logo"/>
+                    </Link>
+
+                    <div className="row">
+                        <div className="col-auto">
+                            <Link to="/" className="navbar-link">Home</Link>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
         </div>
 
         <div className="content">
